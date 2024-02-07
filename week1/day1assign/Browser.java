4357d3e90506eb919ed30dce6c949c1efbb1a91c
Ranjithraj2k <159203000+Ranjithraj2k@users.noreply.github.com>
2024-02-07 05:54:19 +0530

    Add files via upload
    Week1.day1

---
+++ week1/day1assign/Browser.java
@@ -0,0 +1,26 @@
+package week1.day1assign;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String BrowserName="Chrome";
+		boolean pageloaded=true;
+		int NoofBrowser=2;
+		char Browserlogo='C';
+		
+		System.out.println(browserVersion);
+		System.out.println(BrowserName);
+		System.out.println(pageloaded);
+		System.out.println(NoofBrowser);
+		System.out.println(Browserlogo);
+		
+				
+		
+		
+		
+	
+
+	}
+
+}
---
+++ week1/day1assign/FindNumber.java
@@ -0,0 +1,18 @@
+package week1.day1assign;
+
+public class FindNumber {
+	public static void main(String[] args) {
+		int num=45;
+		if (num>0) {
+			System.out.println("The number is positive");
+			
+			
+		}
+		else {
+			System.out.println("The number is negative");
+		}
+		
+		
+	}
+
+}
